2623373ad0e9d30455f54bf428cade3cdcbaf215
Glenn Dixon <110410774+glenxo@users.noreply.github.com>
2023-08-06 23:05:57 -0500

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,15 +7,31 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Music <code>from the</code> gods.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/@glencho1809"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Youtube
+        </a>
+        <a
+          className="App-link"
+          href="https://open.spotify.com/artist/7C56DuVbRo9wBxT54pBl3o/discography"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotify
+        </a>
+        <a
+          className="App-link"
+          href="https://soundcloud.com/glen-cho-406954344"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Soundcloud
         </a>
       </header>
     </div>
